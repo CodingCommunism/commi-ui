17ce4ecd1dea1ed8fc017998c50287685bd2fd84
milkiq <17810390+milkiq@users.noreply.github.com>
2021-07-14 00:50:19 +0800

    feat: modify todo which about md-loader.

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
-  // 实现一个 markdown loader 用于加载特定格式的 markdown 文件，生成对组件示例的预览
+  /**
+   * TODO
+   * 实现一个 markdown loader 用于加载特定格式的 markdown 文件，生成对组件示例的预览
+   * 大概类似于 element-ui 组件库的文档那种吧…
+   * 为什么不用 storybook 之类的？因为实在是太丑了…太丑了…
+  */
 })
